b4aa3e8cc47184d78a3a1b584a808f7b684ddc60
Elton Nix <emaker27@gmail.com>
2015-06-09 19:50:59 -0400

    Added image assets.

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Elton Nix on 6/9/15.
+//  Copyright (c) 2015 Elton Nix. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "")
+    var isRed = true
+}
